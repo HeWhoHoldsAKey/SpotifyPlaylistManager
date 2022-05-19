0fe234995a548a2309222961cfd4f49d66487aa9
HeWhoHoldsAKey <20054042+HeWhoHoldsAKey@users.noreply.github.com>
2022-05-19 09:35:46 -0500

    Update CallBack.java

--- src/main/java/com/spm/main/spring/CallBack.java
+++ src/main/java/com/spm/main/spring/CallBack.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spm.main.SpotifyHdlr;
-
+//Fairly simple is the callback page. Gets the code from the url.
 @RestController
 @RequestMapping(path = "callback")
 public class CallBack {
 	
 	@GetMapping
 	public void callback(HttpServletRequest req) {
+		//Gets the code here very simple
 		String code = req.getQueryString().substring(5);
 		SpotifyHdlr.setCredentials(code);
 	}
