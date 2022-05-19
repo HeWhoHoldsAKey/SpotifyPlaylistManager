948b60f6f1a58d6d2d945a82240b2ff92b33c48c
HeWhoHoldsAKey <20054042+HeWhoHoldsAKey@users.noreply.github.com>
2022-05-19 09:37:26 -0500

    Update Main.java

--- src/main/java/com/spm/main/Main.java
+++ src/main/java/com/spm/main/Main.java
@@ -3,7 +3,7 @@ package com.spm.main;
 import com.spm.main.app.MainWindow;
 
 public class Main {
-
+	//This used to be full now it just opens the gui. makes life easier and more contained... 
 	public static void main(String[] args) {
 		MainWindow.open(args);
 
