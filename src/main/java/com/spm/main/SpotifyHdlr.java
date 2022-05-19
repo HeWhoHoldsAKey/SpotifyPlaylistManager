eef68b669bdcc7e241bba4067edf37270f7a55e1
HeWhoHoldsAKey <20054042+HeWhoHoldsAKey@users.noreply.github.com>
2022-05-19 09:39:45 -0500

    Update SpotifyHdlr.java

--- src/main/java/com/spm/main/SpotifyHdlr.java
+++ src/main/java/com/spm/main/SpotifyHdlr.java
@@ -16,15 +16,19 @@ import se.michaelthelin.spotify.requests.authorization.authorization_code.Author
 import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
 public class SpotifyHdlr {
+	//Yeah so this is fun.. Oauth 2.0 authentication is great...
 	private final static String client_id = "f9c527b8e9c54d399a192f6dbb5fea0d";
 	private final static String clientSecret = "ef2dc527f7a64222af08276edbf079cd";
 	private static String code = "";
-
+	
+	//Callback stuff
 	private final static URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/callback");
 
+	//WHY DOES EVERYTHING HAVE A BUILDER >:(
 	private final static SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(client_id)
 			.setClientSecret(clientSecret).setRedirectUri(redirectUri).build();
 
+	//Yes. i want ALL permissions
 	static AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
 			.scope("user-read-private," + "user-read-email," + "ugc-image-upload," + "user-modify-playback-state,"
 					+ "user-read-playback-state," + "user-read-currently-playing," + "user-follow-modify,"
@@ -34,15 +38,18 @@ public class SpotifyHdlr {
 					+ "user-library-modify," + "user-library-read")
 			.show_dialog(true).build();
 
+	//GeT ThE uRi
 	public static URI getURI() {
 
 		return authorizationCodeUriRequest.getUri();
 	}
 	
+	//This one is actually VERY USEFUL
 	public static SpotifyApi getSAPI() {
 		return spotifyApi;
 	}
 	
+	//Yeah, now reset the credentials... why BECAUSE OAUTH 2.0 AUTH HATES YOU... at least its mildly secure
 	public static void setCredentials(String cd) {
 		code = cd;
 		final AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
